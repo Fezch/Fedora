7c151f485ed41cd1c3dc6f58307f8716ceddc372
root <root@localhost.localdomain>
2013-09-26 19:20:30 -0700

    ex4 finished ready for formatting

--- af4.sh
+++ af4.sh
@@ -4,9 +4,11 @@ if [ -d $1 ]
 	then cd $1
 	if [ -f $2 ]
 		then 
-			echo --- $2 is a file ---
+			echo ---$2 is a file ---; 
+						head -n3 $2
 	else 
-		echo --- not valid file ---
+		echo --- $2 not valid file ---
 	fi
-else echo -------- $1 is not a valid directory --------
+else
+	echo --- $1 is not a valid directory ---
 fi
