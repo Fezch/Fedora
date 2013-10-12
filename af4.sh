feeb4b2df8c2709a228ee5f696f74c6b46e4229f
root <root@localhost.localdomain>
2013-10-12 15:30:49 +1300

    af4

--- af4.sh
+++ af4.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ -d $1 ]
-	then cd $1
-	if [ -f $2 ]
-		then 
-			echo ---$2 is a file ---; 
-						head -n3 $2
-	else 
-		echo --- $2 not valid file ---
-	fi
-else
-	echo --- $1 is not a valid directory ---
-fi
+if [ -n "$1" ]
+	then [ -d $1 ] && cd $1
+		if [ -f $2 ]
+			then 
+				echo ---$2 is a file---;
+			head -n3 $2
+		else 
+			echo --- $2 is not a valid file ---
+		fi
+else 
+	echo --- Please enter a directory when invoking the script  ---
+fi 
