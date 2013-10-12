a713505d0d4b39f0dda01e10038d536a80eadc1c
root <root@localhost.localdomain>
2013-10-13 00:59:32 +1300

    af4 completed

--- af4.sh
+++ af4.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-if [ -n "$1" ]
-	then [ -d $1 ] && cd $1
-		if [ -f $2 ]
-			then 
-				echo ---$2 is a file---;
-			head -n3 $2
-		else 
-			echo --- $2 is not a valid file ---
-		fi
-else 
-	echo --- Please enter a directory when invoking the script  ---
-fi 
+if [ -n "$1" ] && [ -n "$2" ]
+	then if [ -d "$1" ] && cd $1
+			then if [ -f "$2" ]
+					then echo ---$2 exists ---; head -n3 $2
+					
+					else echo --- $2 does not exist in $1 ---
+					fi
+			
+			else echo --- $1 is not a valid directory ---
+			fi
+	
+	else echo --- Please enter a directory and then a file when invoking the script ---
+fi
